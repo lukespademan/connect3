ff01ab5bef2c0462d96e1c8a27ed00151adbbb81
lukespademan <luke.spademan@gmail.com>
2017-10-15 17:21:26 +0000

    fixed winning

--- player1.py
+++ player1.py
@@ -48,42 +48,53 @@ def detect_win(board):
     rows = board.split(":")[:-1]
     won = False
     """checks for 3 in a row"""
-    for row in rows:
-        c = ""
-        for i in range(3):
-            if row[i] != "0":
-                c = row[i]
-                if row[i] == row[i+1] == row[i+2]:
-                    won = True
-                    return won
+    try:
+        for row in rows:
+            c = ""
+            for i in range(3):
+                if row[i] != "0":
+                    c = row[i]
+                    if row[i] == row[i+1] == row[i+2]:
+                        won = True
+                        return won
+    except:
+        pass
     """checks for 3 in a col"""
-    for row in range(3):
-        c = ""
-        for col in range(5):
-            if rows[row][col] != "0":
-                c = rows[row][col]
-                if rows[row][col] == rows[row+1][col] == rows[row+2][col]:
-                    won = True
-                    return won
+    try:
+        for row in range(3):
+            c = ""
+            for col in range(5):
+                if rows[row][col] != "0":
+                    c = rows[row][col]
+                    if rows[row][col] == rows[row+1][col] == rows[row+2][col]:
+                        won = True
+                        return won
+    except:
+        pass
     """checks for 3 in a y=-x+c diag"""
-    for row in range(3):
-        c = ""
-        for col in range(3):
-            if rows[row][col] != "0":
-                c = rows[row][col]
-                if rows[row][col] == rows[row+1][col+1] == rows[row+2][col+2]:
-                    won = True
-                    return won
+    try:
+        for row in range(3):
+            c = ""
+            for col in range(3):
+                if rows[row][col] != "0":
+                    c = rows[row][col]
+                    if rows[row][col] == rows[row+1][col+1] == rows[row+2][col+2]:
+                        won = True
+                        return won
+    except:
+        pass
     """checks for 3 in a y=x+c diag"""
-    for row in range(2, 5):
-        c = ""
-        for col in range(2, 5):
-            if rows[row][col] != "0":
-                c = rows[row][col]
-                if rows[row][col] == rows[row-1][col-1] == rows[row-2][col-2]:
-                    won = True
-                    return won
-
+    try:
+        for row in range(2, 5):
+            c = ""
+            for col in range(2, 5):
+                if rows[row][col] != "0":
+                    c = rows[row][col]
+                    if rows[row][col] == rows[row-1][col-1] == rows[row-2][col-2]:
+                        won = True
+                        return won
+    except:
+        pass
     return won
 
 
