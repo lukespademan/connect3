86098d5e192758b197615e239d4f9070b751cded
lukespademan <luke.spademan@gmail.com>
2017-10-15 14:38:22 +0000

    fixed size of data to send to RPi

--- viewer.py
+++ viewer.py
@@ -9,6 +9,6 @@ radio.on()
 while True:
     data = radio.receive()
     if data:
-        if data != "YT" and len(data) > 20:
+        if data != "YT" and len(data) == 30:
             display.show(Image(data))
             print(data)
