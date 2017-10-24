232bb62d60bd8ce567d9121b173b2fe04d59afae
luke-spademan <luke.spademan@gmail.com>
2017-10-24 13:03:31 +0100

    updated pi code

--- display.py
+++ display.py
@@ -14,42 +14,32 @@ s.databits = serial.EIGHTBITS
 s.stopbits = serial.STOPBITS_ONE
 sense.show_letter("~")
 sleep(0.5)
-p1 = (0, 0, 255)
-p2 = (255, 0, 0)
+player1 = (0, 0, 255)
+player2 = (255, 0, 0)
 blank = (0, 0, 0)
 edge = (100, 100, 100)
 
 while True:
-    try:
-        data = s.readline().decode("UTF-8").rstrip()
-        formated = []
+    data = s.readline().decode("UTF-8").rstrip()
+    data_s = data.split(":")
 
-        count = 0
-        for i in range(8*2):
-            formated.append(edge)
-        for c in data:
-            if c != ":":
-                if count == 0:
-                    formated.append(edge)
-                if c == "3":
-                    formated.append(p2)
-                if c == "9":
-                    formated.append(p1)
-                if c == "0":
-                    formated.append(blank)
-                if count == 4:
-                    formated.append(edge)
-                    formated.append(edge)
-                    count = -1
+    formatted = []  # will store data to send to LED matrix
 
+    for i in range(8*2):  # top two rows
+        formatted.append(edge)
 
-                count += 1
-        for i in range(8):
-            formated.append(edge)
+    for row in data_s:
+        formatted.append(edge)  # first col is an edge
+        for c in row:
+            if c == "4":
+                formatted.append(player2)
+            if c == "9":
+                formatted.append(player1)
+            if c == "0":
+                formated.append(blank)
+        formatted.append(edge)  # two thick edge on right side
+        formatted.append(edge)
 
-        sense.set_pixels(formated)
-    except ValueError as e:
-        print(e)
-
-sense.clear()
-s.close()
+    for i in range(8):
+        formatted.append(edge)
+    sense.set_pixels(formated)
